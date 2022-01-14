5e0273fe3f05a29476b785c253b6bf60d10b1a02
tkyser-rodeo <78568187+tkyser-rodeo@users.noreply.github.com>
2022-01-14 12:25:38 -0600

    Android initial

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import 'react-native-gesture-handler';
 import 'react-native-console-time-polyfill';
 import { AppRegistry } from 'react-native';
-
 import { name as appName, share as shareName } from './app.json';
+import { buildId } from './rox_labs.json';
+import roxLabsStore from './roxLabsStore';
 
 if (__DEV__) {
 	require('./app/ReactotronConfig');
@@ -17,7 +18,22 @@ if (__DEV__) {
 	console.error = () => {};
 	console.info = () => {};
 }
-
+async function getRoxLabsApiData() {
+	const response = await fetch('https://roxchatapp.roxlabs.tech/config.json');
+	const json = await response.json();
+	return json;
+}
+// roxLabsStore.removeItem('rlServerURL');
+getRoxLabsApiData().then(response => {
+	if (buildId > response.prodBuildID) {
+		console.log('using dev url');
+		roxLabsStore.setStringAsync('rlServerURL', response.dev_url);
+	}
+	if (buildId <= response.prodBuildID) {
+		console.log('using prod url');
+		roxLabsStore.setStringAsync('rlServerURL', response.prod_url);
+	}
+});
 AppRegistry.registerComponent(appName, () => require('./app/index').default);
 AppRegistry.registerComponent(shareName, () => require('./app/share').default);
 
