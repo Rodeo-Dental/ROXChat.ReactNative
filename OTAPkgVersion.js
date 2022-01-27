452e5b4ad92eab84b8a6961656cdc150ab7e8910
tkyser-rodeo <78568187+tkyser-rodeo@users.noreply.github.com>
2022-01-27 13:59:52 -0600

    Added channel display to version number
    Signed-off-by: tkyser-rodeo <78568187+tkyser-rodeo@users.noreply.github.com>

--- OTAPkgVersion.js
+++ OTAPkgVersion.js
@@ -2,6 +2,7 @@ import React from 'react';
 import codePush from 'react-native-code-push';
 import { Text } from 'react-native';
 import { getAppVersion } from './OTAMeta';
+import appConfig from './app.json';
 
 class OTAPkgVersion extends React.Component {
 	componentDidMount() {
@@ -9,10 +10,23 @@ class OTAPkgVersion extends React.Component {
 			this.setState({ label: metadata.label, version: metadata.appVersion, description: metadata.description });
 		});
 		this.setState({ appVersionOTA: getAppVersion });
+		const channel = appConfig.activeChannel;
+		let channelDisplay = 'BETA';
+		if (channel === 'prod') {
+			channelDisplay = 'Official';
+		}
+		if (channel == null) {
+			channelDisplay = '?';
+		}
+		this.setState({ channel: channelDisplay });
 	}
 
 	render() {
-		return <Text>{this.state.appVersionOTA}</Text>;
+		return (
+			<Text>
+				{this.state.appVersionOTA} - {this.state.channel}
+			</Text>
+		);
 	}
 }
 
